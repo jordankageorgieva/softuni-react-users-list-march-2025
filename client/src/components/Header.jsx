0e99cc229bb4f4cbcbf85a0c44d03eced03fdc3d
jordankageorgieva <jo_georgieva@abv.bg>
2025-03-07 21:26:24 +0200

    header changed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
     return (
         <header className="header">
             <div className="logo">
-                <span className="course">React Workshop - September 2023</span>
+                <span className="course">React Workshop - March 2025</span>
             </div>
         </header>
     );
